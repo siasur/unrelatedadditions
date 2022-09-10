9de12980b4514c514effb294eee8c85b5e52b383
Siasur <siasur@pm.me>
2022-09-10 02:59:23 +0200

    Great simplification of the Angel Block place logic

--- src/main/java/me/siasur/unrelatedadditions/item/AngelBlockItem.java
+++ src/main/java/me/siasur/unrelatedadditions/item/AngelBlockItem.java
@@ -2,7 +2,6 @@ package me.siasur.unrelatedadditions.item;
 
 import me.siasur.unrelatedadditions.utils.VectorHelper;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -10,14 +9,19 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class AngelBlockItem extends BlockItem {
+
+    public static final String PLACEMENT_HINT_TOOLTIP_KEY = "tooltip.unrelatedadditions.angelblock_placement";
 
     public AngelBlockItem(Block block, Properties properties) {
         super(block, properties);
@@ -26,21 +30,17 @@ public class AngelBlockItem extends BlockItem {
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack itemStack = player.getItemInHand(hand);
-        Vec3 viewVec = player.getViewVector(1);
-        Vec3 target = player.getEyePosition().add(viewVec);
-
-        // I absolutely hate this... But it works, so it's going to stay until I find a better way
-        while (player.getBoundingBox().intersects(new AABB(VectorHelper.floor(target), VectorHelper.floor(target).add(1, 1, 1))))
-        {
-            target = target.add(viewVec);
-        }
-
+        Vec3 target = player.getPosition(1).subtract(0, 1, 0);
         BlockPos position = VectorHelper.toBlockPos(target);
+
         BlockHitResult hitResult = new BlockHitResult(target, player.getDirection(), position, true);
         BlockPlaceContext placeContext = new BlockPlaceContext(level, null, hand, itemStack, hitResult);
-
         InteractionResult placeResult = this.place(placeContext);
-
         return new InteractionResultHolder(placeResult, itemStack);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
+        components.add(Component.translatable(PLACEMENT_HINT_TOOLTIP_KEY));
     }
 }
