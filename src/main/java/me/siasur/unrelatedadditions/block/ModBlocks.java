0efdca2f4c396f6cb2c9d9f2233c3e0b655c78ef
Siasur <siasur@pm.me>
2022-09-10 22:59:10 +0200

    Reordered/Sorted compressed block registration

--- src/main/java/me/siasur/unrelatedadditions/block/ModBlocks.java
+++ src/main/java/me/siasur/unrelatedadditions/block/ModBlocks.java
@@ -26,32 +26,43 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, UnrelatedAdditions.MODID);
     private static final String COMPRESSION_TOOLTIP_KEY = "tooltip.unrelatedadditions.compressedblock";
 
-    public static RegistryObject<Block> COMPRESSED_ANDESITE = registerBlock("compressed_andesite", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.ANDESITE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
-    public static RegistryObject<Block> COMPRESSED_COBBLED_DEEPSLATE = registerBlock("compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
     public static RegistryObject<Block> COMPRESSED_COBBLESTONE = registerBlock("compressed_cobblestone", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
-    public static RegistryObject<Block> COMPRESSED_DIORITE = registerBlock("compressed_diorite", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.DIORITE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
-    public static RegistryObject<Block> COMPRESSED_DIRT = registerBlock("compressed_dirt", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.DIRT)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
-    public static RegistryObject<Block> COMPRESSED_GRANITE = registerBlock("compressed_granite", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.GRANITE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
-    public static RegistryObject<Block> COMPRESSED_GRAVEL = registerBlock("compressed_gravel", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.GRAVEL)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
-    public static RegistryObject<Block> COMPRESSED_RED_SAND = registerBlock("compressed_red_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.RED_SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
-    public static RegistryObject<Block> COMPRESSED_SAND = registerBlock("compressed_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
-    public static RegistryObject<Block> DOUBLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("double_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
     public static RegistryObject<Block> DOUBLE_COMPRESSED_COBBLESTONE = registerBlock("double_compressed_cobblestone", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
-    public static RegistryObject<Block> DOUBLE_COMPRESSED_DIRT = registerBlock("double_compressed_dirt", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.DIRT)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
-    public static RegistryObject<Block> DOUBLE_COMPRESSED_GRAVEL = registerBlock("double_compressed_gravel", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.GRAVEL)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
-    public static RegistryObject<Block> DOUBLE_COMPRESSED_RED_SAND = registerBlock("double_compressed_red_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.RED_SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
-    public static RegistryObject<Block> DOUBLE_COMPRESSED_SAND = registerBlock("double_compressed_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
-    public static RegistryObject<Block> QUADRUPLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("quadruple_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(6561));
-    public static RegistryObject<Block> QUADRUPLE_COMPRESSED_COBBLESTONE = registerBlock("quadruple_compressed_cobblestone", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(6561));
-    public static RegistryObject<Block> QUINTUPLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("quintuple_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(59049));
-    public static RegistryObject<Block> QUINTUPLE_COMPRESSED_COBBLESTONE = registerBlock("quintuple_compressed_cobblestone", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(59049));
-    public static RegistryObject<Block> SEXTUPLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("sextuple_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(531441));
-    public static RegistryObject<Block> SEXTUPLE_COMPRESSED_COBBLESTONE = registerBlock("sextuple_compressed_cobblestone", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(531441));
-    public static RegistryObject<Block> TRIPLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("triple_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(729));
     public static RegistryObject<Block> TRIPLE_COMPRESSED_COBBLESTONE = registerBlock("triple_compressed_cobblestone", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(729));
+    public static RegistryObject<Block> QUADRUPLE_COMPRESSED_COBBLESTONE = registerBlock("quadruple_compressed_cobblestone", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(6561));
+    public static RegistryObject<Block> QUINTUPLE_COMPRESSED_COBBLESTONE = registerBlock("quintuple_compressed_cobblestone", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(59049));
+    public static RegistryObject<Block> SEXTUPLE_COMPRESSED_COBBLESTONE = registerBlock("sextuple_compressed_cobblestone", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(531441));
+
+    public static RegistryObject<Block> COMPRESSED_COBBLED_DEEPSLATE = registerBlock("compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
+    public static RegistryObject<Block> DOUBLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("double_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
+    public static RegistryObject<Block> TRIPLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("triple_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(729));
+    public static RegistryObject<Block> QUADRUPLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("quadruple_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(6561));
+    public static RegistryObject<Block> QUINTUPLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("quintuple_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(59049));
+    public static RegistryObject<Block> SEXTUPLE_COMPRESSED_COBBLED_DEEPSLATE = registerBlock("sextuple_compressed_cobbled_deepslate", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(531441));
+
+    public static RegistryObject<Block> COMPRESSED_DIRT = registerBlock("compressed_dirt", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.DIRT)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
+    public static RegistryObject<Block> DOUBLE_COMPRESSED_DIRT = registerBlock("double_compressed_dirt", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.DIRT)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
     public static RegistryObject<Block> TRIPLE_COMPRESSED_DIRT = registerBlock("triple_compressed_dirt", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.COBBLED_DEEPSLATE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(729));
-    public static RegistryObject<Block> TRIPLE_COMPRESSED_RED_SAND = registerBlock("triple_compressed_red_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.RED_SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(729));
+
+    public static RegistryObject<Block> COMPRESSED_SAND = registerBlock("compressed_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
+    public static RegistryObject<Block> DOUBLE_COMPRESSED_SAND = registerBlock("double_compressed_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
     public static RegistryObject<Block> TRIPLE_COMPRESSED_SAND = registerBlock("triple_compressed_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(729));
+
+    public static RegistryObject<Block> COMPRESSED_RED_SAND = registerBlock("compressed_red_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.RED_SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
+    public static RegistryObject<Block> DOUBLE_COMPRESSED_RED_SAND = registerBlock("double_compressed_red_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.RED_SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
+    public static RegistryObject<Block> TRIPLE_COMPRESSED_RED_SAND = registerBlock("triple_compressed_red_sand", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.RED_SAND)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(729));
+
+    public static RegistryObject<Block> COMPRESSED_GRAVEL = registerBlock("compressed_gravel", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.GRAVEL)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
+    public static RegistryObject<Block> DOUBLE_COMPRESSED_GRAVEL = registerBlock("double_compressed_gravel", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.GRAVEL)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(81));
+
+    public static RegistryObject<Block> COMPRESSED_ANDESITE = registerBlock("compressed_andesite", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.ANDESITE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
+
+    public static RegistryObject<Block> COMPRESSED_DIORITE = registerBlock("compressed_diorite", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.DIORITE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
+
+    public static RegistryObject<Block> COMPRESSED_GRANITE = registerBlock("compressed_granite", ()-> new Block(BlockBehaviour.Properties.copy(Blocks.GRANITE)), ModCreativeModeTab.TAB_UNRELATEDADDITIONS, getCompressionTooltip(9));
+
+
+
 
     public static RegistryObject<AngelBlock> ANGEL_BLOCK = BLOCKS.register("angel_block", () -> new AngelBlock(BlockBehaviour.Properties.copy(Blocks.OBSIDIAN).instabreak().lightLevel((state) -> 8)));
 
